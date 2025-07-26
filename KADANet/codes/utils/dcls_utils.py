f0ba581b4a525801fa5d6662ca4188ac91152c08
Lucky-lili1 <1529681040@qq.com>
2025-07-26 20:27:27 +0800

    Update dcls_utils.py

--- KADANet/codes/utils/dcls_utils.py
+++ KADANet/codes/utils/dcls_utils.py
@@ -30,30 +30,21 @@ def normkernel_to_downkernel(rescaled_blur_hr, rescaled_hr, ksize, eps=1e-10):
     return ker
 
 
+
 # ------------------------------------------------------
 # -----------Constraint Least Square Filter-------------
-#约束最小二乘滤波器
-#使用两个核（kernel 和 grad_kernel）来对图像 img 进行去模糊处理
 def get_uperleft_denominator(img, kernel, grad_kernel):
     ker_f = convert_psf2otf(kernel, img.size()) # discrete fourier transform of kernel
-    # kernel的离散傅里叶变换
-
     ker_p = convert_psf2otf(grad_kernel, img.size()) # discrete fourier transform of kernel
-    # grad_kernel的离散傅里叶变换
 
     denominator = inv_fft_kernel_est(ker_f, ker_p)
-
-
     numerator = torch.rfft(img, 3, onesided=False)
-    # 将图像从空间域转换到频率域
-
     deblur = deconv(denominator, numerator)
     return deblur
 
 
 # --------------------------------
 # --------------------------------
-#计算卷积核的伪逆，主要在频域中进行操作
 def inv_fft_kernel_est(ker_f, ker_p):
     inv_denominator = ker_f[:, :, :, :, 0] * ker_f[:, :, :, :, 0] \
                       + ker_f[:, :, :, :, 1] * ker_f[:, :, :, :, 1] \
@@ -66,8 +57,7 @@ def inv_fft_kernel_est(ker_f, ker_p):
     return inv_ker_f
 
 # --------------------------------
-# --------------------------------？？？？？？？？？？
-#使用伪逆卷积核对输入图像进行去模糊。
+# --------------------------------
 def deconv(inv_ker_f, fft_input_blur):
     # delement-wise multiplication.
     deblur_f = torch.zeros_like(inv_ker_f).cuda()
@@ -80,34 +70,21 @@ def deconv(inv_ker_f, fft_input_blur):
 
 # --------------------------------
 # --------------------------------
-# 旨在将一个点扩散函数（PSF）转换为其对应的光学传递函数（OTF），去模糊
-#这是进行卷积计算的准备工作
-def  convert_psf2otf(ker, size):
-    # 创建一个与给定大小相同的零张量，并将其移动到CUDA设备（如果可用）
+def convert_psf2otf(ker, size):
     psf = torch.zeros(size).cuda()
-
     # circularly shift
     centre = ker.shape[2]//2 + 1
-    ## 首先，我们假设ker是一个四维张量（例如，在批量处理时），但实际上可能只需要处理最后一个两个维度（H, W）
-    #计算中心索引
-
     psf[:, :, :centre, :centre] = ker[:, :, (centre-1):, (centre-1):]
     psf[:, :, :centre, -(centre-1):] = ker[:, :, (centre-1):, :(centre-1)]
     psf[:, :, -(centre-1):, :centre] = ker[:, :, : (centre-1), (centre-1):]
     psf[:, :, -(centre-1):, -(centre-1):] = ker[:, :, :(centre-1), :(centre-1)]
-    #将ker的不同部分复制到psf中，但这并不是循环移位
-    # 它实际上是在重新排列ker的元素，，但这种方式不会将PSF的中心移动到张量的中心
-
     # compute the otf
     otf = torch.rfft(psf, 3, onesided=False)
-    #将这个重新排列的psf视为PSF，并计算其傅里叶变换
     return otf
 
 # --------------------------------
 # --------------------------------
-#将光学传递函数 (OTF) 转换回点扩散函数 (PSF)。
 def convert_otf2psf(otf, size):
-    #循环位移
     ker = torch.zeros(size).cuda()
     psf = torch.irfft(otf, 3, onesided=False)
 
@@ -122,13 +99,10 @@ def convert_otf2psf(otf, size):
 
     return ker
 
-#将卷积核中可以忽略的值归零，以确保卷积核的稀疏性。
+
 def zeroize_negligible_val(k, n=40):
     """Zeroize values that are negligible w.r.t to values in k"""
     # Sort K's values in order to find the n-th largest
-    ## Sort K's values in order to find the n-th largest
-    #“”“将相对于k中的值可忽略不计的值归零”“”
-    #对K的值进行排序，以找到第n个最大的值
     pc = k.shape[-1]//2 + 1
     k_sorted, indices = torch.sort(k.flatten(start_dim=1))
     # Define the minimum value as the 0.75 * the n-th largest value
@@ -141,7 +115,6 @@ def zeroize_negligible_val(k, n=40):
     return norm_k
 
 def postprocess(*images, rgb_range):
-    #对图像进行后处理，包括调整像素范围。
     def _postprocess(img):
         pixel_range = 255 / rgb_range
         return img.mul(pixel_range).clamp(0, 255).round().div(pixel_range)
